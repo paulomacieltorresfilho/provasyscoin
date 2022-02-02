da1c50c02e3ab0047ef0ee2b560f424a2cc17802
Paulo Torres <paulomacieltorresfilho@gmail.com>
2022-02-02 04:10:27 -0300

    arquivos adicionados

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const USER = '"syscoin"';
+const PASSWORD = '"meEscolhe"';
+
+app.get('/provasyscoin', (req, res) => {
+    const { user, password } = req.query;
+
+    if (user !== USER || password !== PASSWORD) res.status(401).send({message: 'Credenciais erradas.'});
+    else res.status(200).send({message: 'Meu nome completo é Paulo Maciel Torres Filho. Eu quero essa vaga!'});
+})
+
+app.listen()
